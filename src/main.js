191fa93ad06f716356641f9f6aeb4c6312da7dfb
mugosagi <mugosagi@gmail.com>
2022-05-21 20:16:42 +0300

    build: update build

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './assets/base.css';
+import './assets/base.scss';
 
 createApp(App).mount('#app');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: 'var(--main-bg-color)',
+        fg: 'var(--main-fg-color)',
+      },
+      fontFamily: {
+        plex: ['IBM Plex Mono', 'monospace'],
+        space: ['Space Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
